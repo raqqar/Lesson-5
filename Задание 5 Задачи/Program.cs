4422035e95e4c590bfae0fd1e5a223374f1749ff
Viacheslav Alekseev <VA_Alekseev@outlook.com>
2021-11-18 02:03:18 +0300

    Добавьте файлы проекта.

---
+++ Задание 5 Задачи/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задание_5_Задачи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сформировать одномерный массив из 7 элементов.
+            //Заполнить массив числами, вводимыми с клавиатуры,
+            //определить среднее арифметическое элементов.
+            int[] array = new int[7];
+            float S = 0;
+            int N = 0;
+            Console.WriteLine("Введите 7 чисел,для заполнения массива");
+            for (int i = 0; i < 7; i++)
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+                array[i]= N;
+                S += array[i];
+            }
+            Console.WriteLine("Среднее арефметическое = {0}",S/7);
+        }
+    }
+}
